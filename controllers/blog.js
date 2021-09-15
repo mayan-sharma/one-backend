5c8bd7e9e370f416614eb8652481434212a735ac
Mayan <delhi.mayansharma@gmail.com>
2021-09-15 14:12:24 +0530

    feat: added update blog route

--- controllers/blog.js
+++ controllers/blog.js
@@ -80,7 +80,7 @@ exports.create = async (req, res) => {
                 }, { transaction });
     
                 const dbCategories = await Category.findAll({ where: { id: categories.split(',') } }, { transaction });
-                const dbTags = await Tag.findAll({ where: { id: categories.split(',') } }, { transaction });
+                const dbTags = await Tag.findAll({ where: { id: tags.split(',') } }, { transaction });
                 
                 await blog.addCategories(dbCategories, { transaction });
                 await blog.addTags(dbTags, { transaction });
@@ -187,5 +187,96 @@ exports.remove = async (req, res) => {
 }
 
 exports.update = async (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+    const form = new formidable.IncomingForm();
+    form.keepExtensions = true;
 
+    try {
+        
+        let oldBlog = await Blog.findOne({
+            where: { slug }
+        });
+
+        if (!oldBlog) {
+            return res.status(400).json({
+                message: 'Blog not found!'
+            });
+        }
+
+        const { fields, files } = await new Promise((resolve, reject) => {
+            form.parse(req, (err, fields, files) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve({ fields, files });
+            });
+        });
+        
+        const { title, body, categories, tags } = fields;
+
+        if (!title || !title.length) {
+            return res.status(400).json({
+                message: 'Title required'
+            });
+        }
+
+        if (!body || body.length < 100) {
+            return res.status(400).json({
+                message: 'Content is too short'
+            });
+        }
+
+        if (!categories || categories.length === 0) {
+            return res.status(400).json({
+                message: 'Atleast 1 category required'
+            });
+        }
+
+        if (!tags || tags.length === 0) {
+            return res.status(400).json({
+                message: 'Atleast 1 tag required'
+            });
+        }
+
+        if (files.photo) {
+            // 1mb
+            if (files.photo.size > 10000000) {
+                return res.status(400).json({
+                    message: 'Image should be less than 1mb'
+                });
+            }
+            oldBlog.photo = fs.readFileSync(files.photo.path);
+        }
+        
+        // to prevent slug from updating
+        let oldSlug = oldBlog.slug;
+        oldBlog = _.merge(oldBlog, fields);
+        oldBlog.slug = oldSlug;
+    
+        oldBlog.excerpt = getExcerpt(body, 320, ' ', '...');
+        oldBlog.metaDesc = stripHtml(body.substring(0, 160)).result;
+
+        await db.transaction(async (transaction) => {
+            try {
+                const blog = await oldBlog.save({ transaction });
+                
+                const dbCategories = await Category.findAll({ where: { id: categories.split(',') } }, { transaction });
+                const dbTags = await Tag.findAll({ where: { id: tags.split(',') } }, { transaction });
+                
+                await blog.setCategories(dbCategories, { transaction });
+                await blog.setTags(dbTags, { transaction });
+
+            } catch (err) {
+                errorHandler(res, err);               
+            }
+        });
+
+        return res.status(200).json({
+            message: 'Blog updated successfully'
+        });
+
+    } catch (err) {
+        errorHandler(res, err);
+    }
 }
