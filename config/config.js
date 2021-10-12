c3df61b122169bd20fa82f72d7f7ff2cbd5c1189
Mayan <delhi.mayansharma@gmail.com>
2021-10-12 21:30:45 +0530

    refactor: added more config variables

--- config/config.js
+++ config/config.js
@@ -2,14 +2,17 @@ require('dotenv').config();
 
 const config = {
     PORT: process.env.PORT,
+    CLIENT_URL: process.env.CLIENT_URL || '',
     DATABASE_USER: process.env.DATABASE_USER || '',
     DATABASE_HOST: process.env.DATABASE_HOST || '',
     DATABASE_PORT: process.env.DATABASE_PORT || '',
     DATABASE_NAME: process.env.DATABASE_NAME || '',
     DATABASE_PASSWORD: process.env.DATABASE_PASSWORD || '',
     JWT_SECRET: process.env.JWT_SECRET?.toString() || '',
+    JWT_PASSWORD_RESET_SECRET: process.env.JWT_PASSWORD_RESET_SECRET?.toString() || '',
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY || '',
-    EMAIL_TO: process.env.EMAIL_TO || ''
+    EMAIL_TO: process.env.EMAIL_TO || '',
+    EMAIL_FROM: process.env.EMAIL_FROM || ''
 };
 
 module.exports = config;
